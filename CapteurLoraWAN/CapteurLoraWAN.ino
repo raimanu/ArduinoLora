ff57d5d856329649f43f3252e52b62f6bdbcc132
Perron Guillaume <guillaume.perron@etudiant.unc.nc>
2022-10-14 10:40:23 +1100

    Modif complexité code

--- CapteurLoraWAN/CapteurLoraWAN.ino
+++ CapteurLoraWAN/CapteurLoraWAN.ino
@@ -1,41 +1,30 @@
-/*
-  First Configuration
-  This sketch demonstrates the usage of MKR WAN 1300/1310 LoRa module.
-  This example code is in the public domain.
-*/
-
 #include <MKRWAN.h>
 #include <Wire.h>
 #include <ArduinoLowPower.h>
-LoRaModem modem; 
-#define address 100   // adresse I2C EC Cond
+#include "Key.h"
+#define address 100   // adresse I2C EC Conductivite
+
+LoRaModem modem;
 
 /// Declaration paramètres LoraWan
-String appEui;
-String appKey;
-String devAddr;
-String nwkSKey;
-String appSKey;
 String messageLora;
 
 /// Declaration capteurs
-int capteurTemperatureEau = A0;   // capteur de temperature de l'eau sur l'entrée analogique A0
-int capteurTemperatureAir = A1;   // capteur de temperature de l'air TMP36  sur l'entrée analogique A1
-int capteurLuminosite = A2;       // capteur de temperature de la luminosité sur l'entrée analogique A2
+int capteurTemperatureEau = A0;   // capteur de temperature de l'eau sur le pin A0
+int capteurTemperatureAir = A1;   // capteur de temperature de l'air TMP36  sur le pin A1
+int capteurLuminosite = A2;       // capteur de temperature de la luminosité sur le pin A2
 
-float temperatureEau;             // Lecture de la mesure de la température de l'eau
-float temperatureAir;             // Lecture de la mesure de la température de l'air
+float temperatureEau;             // Variable température de l'eau
+float temperatureAir;             // Variable température de l'air
 float luminosite;
 
-byte code = 0;       // Sonde EC : 1 octet code d'erreur
+byte code = 0;       // code erreur Sonde EC
 char reponse[48];    // Réponse de la sonde EC (réservation de 48 octets en mémoire)
-byte in_char = 0;    // La lecture de la réponse de la sonde EC se fait caractère par caractère  
-byte i = 0;          // variable positionnelle (reponse)
+byte in_char = 0;
+byte i = 0;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  //!\ Attention --> while (!Serial);
 
   //------- Connection au réseau LoraWan UNC
   
@@ -43,35 +32,30 @@ void setup() {
     Serial.println("Failed to start module");
     while (1) {}
   };
-  Serial.print("Your module version is: ");
+  Serial.print("La version de votre module est : ");
   Serial.println(modem.version());
   if (modem.version() != ARDUINO_FW_VERSION) {
-    Serial.println("Please make sure that the latest modem firmware is installed.");
-    Serial.println("To update the firmware upload the 'MKRWANFWUpdate_standalone.ino' sketch.");
+    Serial.println("Vérifié que la dernière version du firmware est installé.");
+    Serial.println("Pour mettre à jour le firmwaire installé 'MKRWANFWUpdate_standalone.ino' sketch.");
   }
-  Serial.print("Your device EUI is: ");
+  Serial.print("Votre device EUI est : ");
   Serial.println(modem.deviceEUI());
 
-  int connected;
-  devAddr = "00000100";
-  nwkSKey = "c9e170f1beae7d5913ea5ba5d3852643";
-  appSKey = "2204f90ae7d7a3728d12cc629897a546";
-  
   devAddr.trim();
   nwkSKey.trim();
   appSKey.trim();
 
-  connected = modem.joinABP(devAddr, nwkSKey, appSKey);
+  int connected = modem.joinABP(devAddr, nwkSKey, appSKey);
   if (!connected) {
-    Serial.println("Something went wrong; are you indoor? Move near a window and retry");
+    Serial.println("Quelque chose ne vas pas ; êtes vous à l'intérieur ? Allez prés d'une fenêtre et réessayer");
     while (1) {}
   }
 
-  // Set poll interval to 60 secs.
+  // Regler intervalle à 60 sec
   modem.minPollInterval(60);
-  // NOTE: independent of this setting, the modem will
-  // not allow sending more than one message every 2 minutes,
-  // this is enforced by firmware and can not be changed.
+  // NOTE: independent de ce parametre, le modem n'envoie
+  // pas plus de deux messages toutes les 2 minutes,
+  // c'est restreint par le firmware et ne peux être changé.
 
   //Initialisation EC Cond  
    Wire.begin();                 // Initialisation du port I2C
@@ -79,13 +63,13 @@ void setup() {
 
 void loop() {
 
-  /////////// Mesure temperature Eau (sonde Atlas) //////////
+  //------ Mesure temperature Eau (sonde Atlas)------//
   temperatureEau = analogRead(capteurTemperatureEau); // Valeur en sortie du convertisseur A/N
   temperatureEau = 3.3/1023*temperatureEau*1000;      // Tension en mV à l'entrée du convertisseur
   temperatureEau = (0.0512*temperatureEau)-20.5128;         // Formule de calcul issue de la documentation technique
   messageLora = "TE:" + String(temperatureEau) + "/";
 
-  /////////// Mesure temperature Air TMP36 //////////
+  //------ Mesure temperature Air TMP36 ------//
   temperatureAir = analogRead(capteurTemperatureAir); // Valeur brute en sortie du convertisseur A/N
   temperatureAir = 3.3/1023*temperatureAir;           // Tension en V à l'entrée du convertisseur (règle de 3)
   temperatureAir =((temperatureAir*1000)-500)/10;     // Formule de calcul issue de la documentation technique
@@ -94,38 +78,35 @@ void loop() {
   luminosite = analogRead(capteurLuminosite);    // Valeur en sortie du convertisseur A/N
   messageLora += "L:" + String(luminosite) + "/";
 
-  /////////// Commande carte Atlas EC Conductivité //////////
+  //------ Commande carte Atlas EC Conductivité ------//
   Wire.beginTransmission(address);  // activation du composant d'adresse "address" avant transmission (protocole I2C)
   Wire.write("r");                  // demande d'une mesure unitaire sur la sonde EC (Atlas Scientific) 
-      
-  Wire.requestFrom(address,48,1);   // demande de récupération de la mesure stockée dans la sonde I2C
-  code = Wire.read();               // lecture du code d'erreur (1er octet)
+  Wire.endTransmission();
+  delay(600);
+  Wire.requestFrom(address,48,1);   // demande de récupération de la mesure stockée dans la sonde
+  code = Wire.read();               // lecture du code d'erreur
   while (Wire.available()) {        // tant que de la donnée est disponible en entrée sur le port I2C
-   in_char = Wire.read();           // lecture, un part un, des octets suivant (resultat de la mesure)
-   reponse[i] = in_char;            // remplissage du tableau (reponse) 
-   i += 1;                          // incrémentation i+1 (position suivante)
-   if (in_char == 0) {              // si une commande nulle est envoyé : fin de la réponse
-     i = 0;                         // remet le compteur i à 0
-     Wire.endTransmission();        // fin de la transmission I2C
-     break;                         // sortir de la boucle 
-   }
+    in_char = Wire.read();
+    reponse[i] = in_char;
+    i += 1;
+    if (in_char == 0) {              // si une commande nulle est envoyé : fin de la réponse
+      i = 0;
+      break;
+    }
   }
-  //messageLora += "EC:" + String(reponse) + "/";
+  
   messageLora += "EC:" + String(reponse).substring(0,5) + "/";
   Serial.println("Taille message : " + messageLora.length());
   Serial.println(messageLora);
 
-  int err;
   modem.setPort(3);
   modem.beginPacket();
   modem.print(messageLora);
-  err = modem.endPacket(true);
+  int err = modem.endPacket(true);
   if (err > 0) {
-    Serial.println("Message sent correctly!");
+    Serial.println("Message envoyé correctement!");
   } else {
-    Serial.println("Error sending message :(");
-    Serial.println("(you may send a limited amount of messages per minute, depending on the signal strength");
-    Serial.println("it may vary from 1 message every couple of seconds to 1 message every minute)");
+    Serial.println("Erreur envoi message");
   }
   LowPower.deepSleep(2000 * 60);
   setup();
---
+++ CapteurLoraWAN/Key.h
@@ -0,0 +1,3 @@
+String devAddr = "00000100";
+String nwkSKey = "c9e170f1beae7d5913ea5ba5d3852643";
+String appSKey = "2204f90ae7d7a3728d12cc629897a546";
