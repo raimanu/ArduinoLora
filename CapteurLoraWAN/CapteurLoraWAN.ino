6b471140d56178507d252e56455e78232ecd8644
Perron Guillaume <guillaume.perron@etudiant.unc.nc>
2022-10-18 15:22:28 +1100

    Ajout Capteur Ph et capteur Hauteur

--- CapteurLoraWAN/CapteurLoraWAN.ino
+++ CapteurLoraWAN/CapteurLoraWAN.ino
@@ -2,29 +2,33 @@
 #include <Wire.h>
 #include <ArduinoLowPower.h>
 #include "Key.h"
-#define address 100   // adresse I2C EC Conductivite
+#define addressEC 100   // adresse I2C EC Conductivite
+#define addressPh 99    // adresse I2C Ph
 
 LoRaModem modem;
 
-/// Declaration paramètres LoraWan
+// Declaration paramètres LoraWan
 String messageLora;
 
-/// Declaration capteurs
-int capteurTemperatureEau = A0;   // capteur de temperature de l'eau sur le pin A0
-int capteurTemperatureAir = A1;   // capteur de temperature de l'air TMP36  sur le pin A1
-int capteurLuminosite = A2;       // capteur de temperature de la luminosité sur le pin A2
+// Declaration capteurs
+const int capteurTemperatureEau = A0;   // temperature de l'eau : A0
+const int capteurTemperatureAir = A1;   // temperature de l'air TMP36 : A1
+const int capteurLuminosite = A2;       // luminosité : A2
 
-float temperatureEau;             // Variable température de l'eau
-float temperatureAir;             // Variable température de l'air
-float luminosite;
+// Capteur ULtrasons broche Trigg et Echo
+const byte TRIGGER_PIN = 2;
+const byte ECHO_PIN = 3;
 
-byte code = 0;       // code erreur Sonde EC
-char reponse[48];    // Réponse de la sonde EC (réservation de 48 octets en mémoire)
-byte in_char = 0;
-byte i = 0;
+byte codeError = 0;       // code erreur sondes Atlas
+char reponse[48];    // réponse des sondes Atlas (48 octets)
 
 void setup() {
   Serial.begin(9600);
+
+  /* Initialise les broches */
+  pinMode(TRIGGER_PIN, OUTPUT);
+  digitalWrite(TRIGGER_PIN, LOW); // La broche TRIGGER doit être à LOW au repos
+  pinMode(ECHO_PIN, INPUT);
 
   //------- Connection au réseau LoraWan UNC
   
@@ -40,10 +44,6 @@ void setup() {
   }
   Serial.print("Votre device EUI est : ");
   Serial.println(modem.deviceEUI());
-
-  devAddr.trim();
-  nwkSKey.trim();
-  appSKey.trim();
 
   int connected = modem.joinABP(devAddr, nwkSKey, appSKey);
   if (!connected) {
@@ -61,30 +61,15 @@ void setup() {
    Wire.begin();                 // Initialisation du port I2C
 }
 
-void loop() {
-
-  //------ Mesure temperature Eau (sonde Atlas)------//
-  temperatureEau = analogRead(capteurTemperatureEau); // Valeur en sortie du convertisseur A/N
-  temperatureEau = 3.3/1023*temperatureEau*1000;      // Tension en mV à l'entrée du convertisseur
-  temperatureEau = (0.0512*temperatureEau)-20.5128;         // Formule de calcul issue de la documentation technique
-  messageLora = "TE:" + String(temperatureEau) + "/";
-
-  //------ Mesure temperature Air TMP36 ------//
-  temperatureAir = analogRead(capteurTemperatureAir); // Valeur brute en sortie du convertisseur A/N
-  temperatureAir = 3.3/1023*temperatureAir;           // Tension en V à l'entrée du convertisseur (règle de 3)
-  temperatureAir =((temperatureAir*1000)-500)/10;     // Formule de calcul issue de la documentation technique
-  messageLora += "TA:" + String(temperatureAir) + "/";
-
-  luminosite = analogRead(capteurLuminosite);    // Valeur en sortie du convertisseur A/N
-  messageLora += "L:" + String(luminosite) + "/";
-
-  //------ Commande carte Atlas EC Conductivité ------//
-  Wire.beginTransmission(address);  // activation du composant d'adresse "address" avant transmission (protocole I2C)
-  Wire.write("r");                  // demande d'une mesure unitaire sur la sonde EC (Atlas Scientific) 
+String requestSondeAtlas(int addrSonde) {
+  Wire.beginTransmission(addrSonde);  // activation du composant d'adresse "address" avant transmission (protocole I2C)
+  Wire.write("r");                  // demande d'une mesure unitaire sur la sonde Atlas Scientific 
   Wire.endTransmission();
   delay(600);
-  Wire.requestFrom(address,48,1);   // demande de récupération de la mesure stockée dans la sonde
-  code = Wire.read();               // lecture du code d'erreur
+  Wire.requestFrom(addrSonde,48,1);   // demande de récupération de la mesure stockée dans la sonde
+  codeError = Wire.read();
+  byte in_char = 0;
+  byte i = 0;
   while (Wire.available()) {        // tant que de la donnée est disponible en entrée sur le port I2C
     in_char = Wire.read();
     reponse[i] = in_char;
@@ -94,10 +79,58 @@ void loop() {
       break;
     }
   }
+  return String(reponse);
+}
+
+void sleepEC(){
+  Wire.beginTransmission(addressEC);
+  Wire.write("SLEEP");
+  Wire.endTransmission();
+}
+
+void sleepPH(){
+  Wire.beginTransmission(addressPh);
+  Wire.write("SLEEP");
+  Wire.endTransmission();
+}
+
+void loop() {
+
+  //------ Mesure temperature Eau (sonde Atlas)------//
+  float temperatureEau = analogRead(capteurTemperatureEau); // Valeur en sortie du convertisseur A/N
+  temperatureEau = 3.3/1023*temperatureEau*1000;      // Tension en mV à l'entrée du convertisseur
+  temperatureEau = (0.0512*temperatureEau)-20.5128;         // Formule de calcul issue de la documentation technique
+  messageLora = "TE:" + String(temperatureEau) + "/";
+
+  //------ Mesure temperature Air TMP36 ------//
+  float temperatureAir = analogRead(capteurTemperatureAir); // Valeur brute en sortie du convertisseur A/N
+  temperatureAir = 3.3/1023*temperatureAir;           // Tension en V à l'entrée du convertisseur (règle de 3)
+  temperatureAir =((temperatureAir*1000)-500)/10;     // Formule de calcul issue de la documentation technique
+  messageLora += "TA:" + String(temperatureAir) + "/";
+
+  int luminosite = analogRead(capteurLuminosite);    // Valeur en sortie du convertisseur A/N
+  messageLora += "L:" + String(luminosite) + "/";
+
+  //------ Commande carte Atlas EC ------//
   
-  messageLora += "EC:" + String(reponse).substring(0,5) + "/";
-  Serial.println("Taille message : " + messageLora.length());
+  messageLora += "EC:" + requestSondeAtlas(addressEC).substring(0,4) + "/";
+
+  //------ Commande carte Atlas Ph ------//
+
+  messageLora += "Ph:" + requestSondeAtlas(addressPh).substring(0,4) + "/";
+
+  //------ Mesure distance capteur ultrasons -------//
+
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN, LOW);
+  long measure = pulseIn(ECHO_PIN, HIGH, 25000UL);
+  float distance_cm = (measure * 0.34 / 2.0)/ 10.0;
+  messageLora += "H:" + String(distance_cm);
+
+  Serial.println("Taille message : " + String(messageLora.length()));
   Serial.println(messageLora);
+  //------ Transmission LoRaWAN ------//
 
   modem.setPort(3);
   modem.beginPacket();
@@ -108,6 +141,13 @@ void loop() {
   } else {
     Serial.println("Erreur envoi message");
   }
-  LowPower.deepSleep(2000 * 60);
-  setup();
+
+  //------ Mise en Veille ------//
+
+  sleepEC();
+  sleepPH();
+  //LowPower.deepSleep(2000 * 60);
+  //setup();
+  delay(2000*60);
 }
+
